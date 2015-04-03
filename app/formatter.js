437751c59761f2a0109dccbec211de4b8596f65c
Jack Dalton <jackdalton@users.noreply.github.com>
2015-04-02 19:05:59 -0600

    Create formatter.js

---
+++ app/formatter.js
@@ -0,0 +1,33 @@
+document.getElementById("formatButton").addEventListener("click", format, false);
+document.getElementById("selectAllButton").addEventListener("click", selectAll, false);
+function format() {
+    var input = document.getElementById("json-input").value;
+    try {
+        input = JSON.parse(input);
+        input = JSON.stringify(input, null, 4);
+        document.getElementById("json-input").value = input;
+        notifyCompletion("done");
+    } catch (error) {
+        notifyCompletion("failed");
+        document.getElementById("errorOut").innerHTML = error;
+    }
+}
+function selectAll() {
+    document.getElementById("json-input").select();
+}
+function notifyCompletion(status) {
+    if (status == "done") {
+        document.getElementById("notification-area").innerHTML = "JSON formatted!";
+        $("#notification-area").fadeTo("fast", 1);
+        setTimeout(function () {
+            $("#notification-area").fadeTo("slow", 0);
+        }, 4500);
+    } else if (status == "failed") {
+        document.getElementById("notification-area").innerHTML = "Something went wrong, see below for details.";
+        $("#notification-area").fadeTo("fast", 1);
+        setTimeout(function () {
+            $("#notification-area").fadeTo("slow", 0);
+            $("#errorOut").fadeTo("slow", 0);
+        }, 4500);
+    }
+}
