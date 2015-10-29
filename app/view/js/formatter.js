de2f770af8916875651619d6ed6a3afcde9347e6
Jack Dalton <jackdalton00@gmail.com>
2015-10-29 13:59:51 +0000

    Staging for something big.

--- app/view/js/formatter.js
+++ app/view/js/formatter.js
@@ -1,36 +1,38 @@
-document.getElementById("formatButton").addEventListener("click", format, false);
-document.getElementById("selectAllButton").addEventListener("click", selectAll, false);
-function format() {
-    var input = document.getElementById("json-input").value;
-    try {
-        input = JSON.parse(input);
-        input = JSON.stringify(input, null, 4);
-        document.getElementById("json-input").value = input;
-        notify(true);
-    } catch (error) {
-        notify(false, error);
+(function() {
+    document.getElementById("formatButton").addEventListener("click", format, false);
+    document.getElementById("selectAllButton").addEventListener("click", selectAll, false);
+    function format() {
+        var input = document.getElementById("json-input").value;
+        try {
+            input = JSON.parse(input);
+            input = JSON.stringify(input, null, 4);
+            document.getElementById("json-input").value = input;
+            notify(true);
+        } catch (error) {
+            notify(false, error);
+        }
     }
-}
-function selectAll() {
-    document.getElementById("json-input").select();
-	document.execCommand("Copy");
-	notify("msg", "Copied!");
-}
-function notify(status, msg) {
-    if (status == "msg") {
-		document.getElementById("notification-area").innerHTML = msg;
-	} else if (status) {
-        document.getElementById("notification-area").innerHTML = "JSON formatted and validated!";
-        $("#notification-area").fadeTo("fast", 1);
-        setTimeout(function () {
-            $("#notification-area").fadeTo("slow", 0);
-        }, 4500);
-    } else if (!status) {
-        document.getElementById("notification-area").innerHTML = "<b>" + msg + "</b>";
-        $("#notification-area").fadeTo("fast", 1);
-        setTimeout(function () {
-            $("#notification-area").fadeTo("slow", 0);
-            $("#errorOut").fadeTo("slow", 0);
-        }, 10000);
+    function selectAll() {
+        document.getElementById("json-input").select();
+    	document.execCommand("Copy");
+    	notify("msg", "Copied!");
     }
-}
+    function notify(status, msg) {
+        if (status == "msg") {
+    		document.getElementById("notification-area").innerHTML = msg;
+    	} else if (status) {
+            document.getElementById("notification-area").innerHTML = "JSON formatted and validated!";
+            $("#notification-area").fadeTo("fast", 1);
+            setTimeout(function () {
+                $("#notification-area").fadeTo("slow", 0);
+            }, 4500);
+        } else if (!status) {
+            document.getElementById("notification-area").innerHTML = "<b>" + msg + "</b>";
+            $("#notification-area").fadeTo("fast", 1);
+            setTimeout(function () {
+                $("#notification-area").fadeTo("slow", 0);
+                $("#errorOut").fadeTo("slow", 0);
+            }, 10000);
+        }
+    }
+})();
